a938b79a4991022c65c5bd0b051549f902e1ee92
Nataly <naty7160@gmail.com>
2024-10-20 16:48:19 -0300

    first commit

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Javascript?',
+    'JavaScript é uma linguagem de programação que permite a criação de páginas web interativas, com elementos dinâmicos e atualizados. '
+)
+
+criaCartao(
+    'Geografia',
+    'O que é a Globalização?',
+    'A globalização é um processo de integração econômica, cultural, política e social entre países, que se caracteriza por um aumento na troca de informações, mercadorias, pessoas e capitais. '
+)
+
+criaCartao(
+    'Química',
+    'O que é um composto químico?',
+    'Um composto orgânico é uma substância química que contém carbono (C) e ligações covalentes (C-H).'
+)
+
+criaCartao(
+    'Lingua Portuguesa',
+    'O que é Concordância nominal ?',
+    'Concordância nominal é a relação de harmonização de gênero e número entre um substantivo e os termos que o modificam, como adjetivos, artigos, pronomes e numerais. '
+)
